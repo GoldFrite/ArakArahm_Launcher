7f8dba9fd82f1169bb037168a49876293c1c9428
GoldFrite <61522145+GoldFrite@users.noreply.github.com>
2021-07-15 13:44:58 +0200

    Add files via upload

---
+++ assets/js/scripts_app.js
@@ -0,0 +1,162 @@
+// Importations
+const { shell } = require("electron");
+const ipc = require("electron").ipcRenderer;
+const util = require('minecraft-server-util');
+// --------------------
+
+// Avertissement console
+console.log("%c⚠ Attends !", "color: red; font-size: 28px; font-weight: 500; font-family: 'Roboto', sans-serif; text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5)");
+console.log("%cNe tape rien ici à moins que tu sois sûr(e) de ce que tu fais !", "color: red; font-size: 17px; font-weight: 500; font-family: 'Roboto', sans-serif; text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);");
+console.log("%cCopier-coller quelque chose dictée par un inconnu risque fortement d'être une arnaque !", "color: red; font-size: 17px; font-weight: 500; font-family: 'Roboto', sans-serif; text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5)");
+// --------------------
+
+// Co OK
+console.log("%c[Launcher]" + "%c [Connexion]" + "%c Connection successful!", "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100")
+
+// Var HTML
+const accountInfos = document.getElementById('account-infos');
+const status = document.getElementById("status");
+const players = document.getElementById("players");
+const logoutButton = document.getElementById("logout-button")
+// --------------------
+
+// Infos du compts
+document.getElementById("account-pseudo").innerHTML = "<b>" + localStorage.getItem("name") + "</b>";
+document.getElementById("account-email").innerHTML = localStorage.getItem("email");
+document.getElementById("account-skin").src = "https://minotar.net/avatar/" + localStorage.getItem("name");
+// --------------------
+
+// Pour masquer AccountInfos
+document.getElementById('account-icon').addEventListener('click', () => {
+    if (getComputedStyle(accountInfos).display != "none") {
+        accountInfos.style = "display: none"
+    } else {
+        accountInfos.style = "display: block"
+    }
+});
+// --------------------
+
+// Jouer
+document.getElementById("play-button").addEventListener("click", () => {
+    ipc.send("play", { u: localStorage.getItem("username"), })
+})
+
+// UUID
+var ifUUID = 0
+document.getElementById("uuid-button").addEventListener("click", () => {
+
+    if (ifUUID === 0) {
+
+        document.getElementById("uuid").innerHTML = "<input type='text' value='" + localStorage.getItem("uuid") + "' class='uuid-input' id='uuid-input' disabled='disabled'><button class='uuid-copy-button hover-shadow' id='uuid-copy-button'>Copier</button>"
+        ifUUID = 1
+        const uuidCopyButton = document.getElementById("uuid-copy-button");
+        const uuidInput = document.getElementById("uuid-input");
+
+        uuidCopyButton.addEventListener("click", () => {
+            uuidInput.disabled = false
+            uuidInput.select()
+            document.execCommand("copy");
+            uuidInput.disabled = true
+            console.log("%c[Launcher]" + "%c [Account]" + "%c UUID copied!", "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            uuidCopyButton.style.background = "#245429";
+            uuidInput.style.borderColor = "#245429"
+
+            function uuidColor() {
+                uuidCopyButton.style.background = "rgb(61, 61, 61)";
+                uuidInput.style.borderColor = "rgb(61, 61, 61)"
+            }
+            setTimeout(uuidColor, 1500)
+        })
+
+    } else {
+        document.getElementById("uuid").innerHTML = ""
+        ifUUID = 0
+    }
+});
+// --------------------
+
+// Logout
+logoutButton.addEventListener("click", () => {
+    ipc.send("logout", {aToken: localStorage.getItem("accessToken"), cToken: localStorage.getItem("clientToken")})
+    localStorage.clear();
+  });
+// --------------------
+
+
+// News
+document.getElementById("more-news-button").addEventListener("click", () => {
+    ipc.send("news")
+})
+// --------------------
+
+// Settings
+document.getElementById("settings-button").addEventListener("click", () => {
+    ipc.send("settings")
+});
+// --------------------
+
+// Diapo
+// document.getElementById("full-diapo-button").addEventListener("click", () => {
+//     ipc.send("diapo")
+// });
+// --------------------
+
+// Ouverture liens
+document.getElementById('account-button').addEventListener('click', () => {
+    shell.openExternal("https://minecraft.net/fr-fr/profile/");
+});
+
+// document.getElementById('site-button').addEventListener('click', () => {
+//     shell.openExternal("https://www.pentagone2.hostim.me/")
+// });
+
+document.getElementById('discord-button').addEventListener('click', () => {
+    shell.openExternal("https://discord.gg/QZeCr9Z8fa")
+});
+
+document.getElementById('twitter-button').addEventListener('click', () => {
+    shell.openExternal("https://discord.gg/QZeCr9Z8fa")
+});
+
+// document.getElementById('insta-button').addEventListener('click', () => {
+//     shell.openExternal("https://www.instagram.com/pentagone2_/")
+// });
+
+// document.getElementById('yt-button').addEventListener('click', () => {
+//     shell.openExternal("https://www.youtube.com/channel/UCUAzUO3TnBZ8AanSOHDaoaQ")
+// });
+
+// document.getElementById('help-discord-button').addEventListener('click', () => {
+//     shell.openExternal("https://discord.com/invite/FePaQ7v");
+// });
+// --------------------
+
+// Status du serveur
+
+servStatus()
+
+document.getElementById("status-refresh").addEventListener("click", () => {
+    servStatus()
+})
+
+function servStatus() {
+
+    util.status('92.152.117.23', { port: 25565 })
+        .then((response) => {
+
+            var playersJSON = JSON.stringify(response.onlinePlayers)
+
+            console.log(response)
+            console.log("%c[Server]" + "%c [Status] " + "%cOnline", "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            console.log("%c[Server]" + "%c [Players] " + "%c" + playersJSON, "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            status.innerHTML = '<b>Status :</b> 🟢 En ligne'
+            players.innerHTML = '<b>Joueurs :</b> ' + playersJSON
+        })
+        .catch(function (error) {
+            console.log("%c[Server]" + "%c [Status] " + "%c" + error, "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            status.innerHTML = '<b>Status :</b> 🔴 Hors ligne';
+            players.innerHTML = '<b>Joueurs :</b> ::'
+        });
+
+}
+// --------------------
---
+++ assets/js/scripts_launch.js
@@ -0,0 +1,326 @@
+// Importations
+const { Client, Authenticator } = require("minecraft-launcher-core");
+const launcher = new Client();
+const ipc = require("electron").ipcRenderer;
+const os = require("os");
+const fs = require("fs");
+let axios = require('axios');
+const request = require('request');
+const unzipper = require("unzipper");
+// --------------------
+
+// Var HTML
+const progressBar = document.getElementById("progress-bar")
+const loadingText = document.getElementById("loading")
+const errorText = document.getElementById("error")
+// --------------------
+
+// Var
+var modpackLink = null
+var fullData
+var xVal = 0
+var isDl = 0
+var errorVar
+let launchOpts
+var modpack = {
+    method: 'post',
+    url: 'http://minecraft-launcher.medianewsonline.com/servers/arakarahm/update/update.json',
+};
+// --------------------
+
+axios(modpack)
+    .then(function (response) {
+        modpackLink = response.data.game
+    })
+    .catch(function (error) {
+        console.log("%c[Launcher]" + "%c [Updater]" + "%c Unable to load the Modpack ZIP file : " + error, "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+    })
+
+ipc.on("data", (event, data) => {
+
+    fullData = data
+
+    try { // Suppr le contenu du dossier mods s'il existe
+        fs.readdir(fullData.appData + "/.arakarahm/mods/", (err, files) => {
+            try {
+                files.forEach(file => {
+                    fs.unlink(fullData.appData + "/.arakarahm/mods/" + file, (err) => { });
+                })
+            } catch (error) { }
+        })
+    } catch (error) { }
+
+    try {
+        fs.unlink(fullData.appData + "/.arakarahm/forge.jar", (err) => { })
+    } catch (error) { }
+
+    if (os.platform == "win32") { // Si l'OS est Win, télécharger JAVA s'il n'existe pas
+
+        fs.access(fullData.appData + "/.arakarahm/runtime/bin/java.exe", (err) => {
+
+            if (err) {
+                changeDlJava();
+            }
+        })
+
+    } else {
+        launch()
+    }
+
+
+    if (os.platform == "win32") { // Opts pour Win
+
+        if (!fs.existsSync(fullData.appData + "/.arakarahm/")) { // Check si JAVA est déja présent
+            fs.mkdirSync(fullData.appData + "/.arakarahm/", { recursive: true })
+            fs.mkdirSync(fullData.appData + "/.arakarahm/runtime/", { recursive: true })
+            changeDlJava()
+        } else if (!fs.existsSync(fullData.appData + "/.arakarahm/runtime")) {
+            fs.mkdirSync(fullData.appData + "/.arakarahm/runtime/", { recursive: true })
+            changeDlJava()
+        } else if (!fs.existsSync(fullData.appData + "/.arakarahm/runtime/bin/java.exe")) {
+            changeDlJava()
+        } else {
+            setTimeout(launch, 1000)
+        }
+
+        launchOpts = { //////////////////////////////
+            javaPath: fullData.appData + "/.arakarahm/runtime/bin/java.exe",
+            authorization: Authenticator.getAuth(localStorage.getItem("username")),
+            root: fullData.appData + "/.arakarahm",
+            removePackage: true,
+            version: {
+                number: "1.7.10",
+                type: "release"
+            },
+            forge: fullData.appData + "/.arakarahm/forge.jar",
+            memory: {
+                max: localStorage.getItem("maxRamValue"),
+                min: localStorage.getItem("minRamValue")
+            }
+        }
+
+    } else { // Opts pour macOS
+
+        launchOpts = { //////////////////////////////
+            authorization: Authenticator.getAuth(localStorage.getItem("username")),
+            root: fullData.appData + "/.arakarahm",
+            removePackage: true,
+            version: {
+                number: "1.7.10",
+                type: "release"
+            },
+            forge: fullData.appData + "/.arakarahm/forge.jar",
+            memory: {
+                max: localStorage.getItem("maxRamValue"),
+                min: localStorage.getItem("minRamValue")
+            }
+        }
+
+    }
+
+    launcher.on("debug", (e) => {
+
+        console.log(e)
+        if (e.includes("[MCLC]: Launching with arguments")) { // Ce message indique que le jeu est télécharge, je change donc l'apparence du HTML
+            loadingText.style.opacity = "0"
+            progressBar.style.opacity = "0"
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.innerHTML = "Lancement du Jeu..."
+                loadingText.style.opacity = "1"
+            }
+        }
+
+        if (!e.includes("[MCLC]: Downloaded assets") && e.includes("[MCLC]: Attempting to download assets")) { // Ces messages indiquent que le jeu n'est pas déja téléchargé, donc je change l'apparence du HTML
+            loadingText.style.opacity = "0"
+            progressBar.style.opacity = "0"
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.innerHTML = "Téléchargement du Jeu..."
+                loadingText.style.opacity = "1"
+                progressBar.style.opacity = "1"
+                progressBar.value = "0"
+            }
+            launcher.on("progress", (e) => {
+                xVal += 1
+                progressBar.max = e.total
+                progressBar.value = e.task
+
+            })
+        }
+    })
+
+    launcher.on("data", (e) => {
+        console.log(e)
+
+        if (e.includes("[Render thread/INFO]")) {
+            if (localStorage.getItem("keepLauncherOpen") && localStorage.getItem("keepLauncherOpen") == "false") {
+                ipc.send("quit")
+            } else {
+                ipc.send("backapp")
+            }
+        }
+    })
+
+    launcher.on("progress", (e) => {
+        console.log(e)
+        progressBar.value = "0"
+        if (e.type == "forge" || e.type == "classes-maven-custom" || e.type == "classes" || e.type == "classes-custom") {
+            xVal += 1
+            progressBar.max = "109"
+            progressBar.value = "" + xVal
+            loadingText.innerHTML = "Vérification du Jeu..."
+        }
+    })
+
+})
+
+// Apparence HTML
+function launch() {
+
+    loadingText.style.opacity = "0"
+    progressBar.style.opacity = "0"
+    setTimeout(change, 1000)
+    function change() {
+        loadingText.innerHTML = "Téléchargement du Jeu..."
+        loadingText.style.opacity = "1"
+        progressBar.style.opacity = "1"
+        progressBar.value = "0"
+    }
+
+    download(modpackLink, fullData.appData + "/.arakarahm/ModPack.zip", (err) => {
+
+        if (err || isDl == -1) {
+
+            loadingText.style.opacity = 0
+            errorText.style.opacity = 0
+            progressBar.style.opacity = 0
+            console.error("%c[Launcher]" + "%c [ModPack]" + "%c Download failed! " + errorVar, "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.innerHTML = "Echec du téléchargement."
+                errorText.innerHTML = "Impossible de télécharger le ModPack."
+                loadingText.style.opacity = 1
+                errorText.style.opacity = 1
+            }
+            isDl = -1
+
+        } else {
+
+            progressBar.style.opacity = 0
+            console.log("%c[Launcher]" + "%c [ModPack]" + "%c Download complete!", "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.style.opacity = 1
+                fs.createReadStream(fullData.appData + '/.arakarahm/ModPack.zip')
+                    .pipe(unzipper.Extract({ path: fullData.appData + "/.arakarahm/" }, { end: setTimeout(lanuchOkAndDelete, 10000) }))
+                function lanuchOkAndDelete() {
+                    setTimeout(launchOk, 2500)
+                    fs.unlink(fullData.appData + "/.arakarahm/ModPack.zip", (err) => { })
+                }
+            }
+            isDl = 0
+        }
+    })
+
+    function launchOk() {
+        launcher.launch(launchOpts)
+        loadingText.style.opacity = "0"
+        setTimeout(change, 1000)
+        function change() {
+            loadingText.innerHTML = "Vérification du Jeu..."
+            loadingText.style.opacity = "1"
+        }
+    }
+
+
+}
+
+function changeDlJava() {
+    setTimeout(change, 1000)
+    function change() {
+        loadingText.innerHTML = "Téléchargement de Java"
+        loadingText.style.opacity = 1
+        errorText.style.opacity = 0
+    }
+
+    download("https://www.dropbox.com/s/262kaub0ra1ma3d/java.zip?dl=1", fullData.appData + "/.arakarahm/runtime/jre.zip", (err) => {
+
+        if (err || isDl == -1) {
+
+            loadingText.style.opacity = 0
+            errorText.style.opacity = 0
+            progressBar.style.opacity = 0
+            console.error("%c[Launcher]" + "%c [Java]" + "%c Download failed! " + errorVar, "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.innerHTML = "Echec du téléchargement."
+                errorText.innerHTML = "Impossible de télécharger Java."
+                loadingText.style.opacity = 1
+                errorText.style.opacity = 1
+            }
+            isDl = -1
+
+        } else {
+
+            loadingText.style.opacity = 0
+            progressBar.style.opacity = 0
+            console.log("%c[Launcher]" + "%c [Java]" + "%c Download complete!", "color: blue; font-weight: 1000", "color: black; font-weight: 700", "color: black; font-weight: 100");
+            setTimeout(change, 1000)
+            function change() {
+                loadingText.innerHTML = "Installation de Java"
+                loadingText.style.opacity = 1
+                fs.createReadStream(fullData.appData + '/.arakarahm/runtime/jre.zip')
+                    .pipe(unzipper.Extract({ path: fullData.appData + "/.arakarahm/runtime/" }, { end: setTimeout(lanuchAndDelete, 10000) }))
+                function lanuchAndDelete() {
+                    setTimeout(launch, 1750)
+                    fs.unlink(fullData.appData + "/.arakarahm/runtime/jre.zip", (err) => { })
+                }
+            }
+            isDl = 0
+        }
+    })
+}
+// --------------------
+
+// Télécharger
+function download(url, dest, cb) {
+
+    const file = fs.createWriteStream(dest)
+    const sendReq = request.get(url)
+    var weight = null
+    var dl = 0
+    progressBar.style.opacity = 1
+
+    sendReq.on('response', (response) => {
+        if (response.statusCode !== 200) {
+            isDl = -1
+        } else {
+            weight = response.headers['content-length'];
+            progressBar.max = weight
+            response.on("data", (chunk) => {
+                dl = dl + chunk.length
+                progressBar.value = dl
+            })
+        }
+    })
+    sendReq.on('error', (err) => {
+        cb(err.message)
+        errorVar = err
+        errorText.style.opacity = 1
+        isDl = -1
+        fs.unlink(dest)
+    })
+    file.on('error', (err) => {
+        cb(err.message);
+        errorVar = err
+        errorText.style.opacity = 1
+        isDl = -1
+        fs.unlink(dest)
+    })
+    sendReq.pipe(file)
+    file.on('finish', () => {
+        file.close(cb);
+    })
+
+}
